4963568bf329923eafd431bd387243971be2bd36
Alison Loffi <alison.loffi@areacentral.com.br>
2024-01-24 13:17:12 -0300

    Mudança da biblioteca de calendario

--- src/views/verificador_pontos/VerificadorPontos.vue
+++ src/views/verificador_pontos/VerificadorPontos.vue
@@ -36,8 +36,12 @@
       </div>
     </Offcanvas>
 
-    <Panel header="Eventos" :class="{'is-light-mode': qalendarLightMode}" class="mt-3">
+    <!-- <Panel header="Eventos" :class="{'is-light-mode': qalendarLightMode}" class="mt-3">
       <Qalendar :events="events" :config="config" />
+    </Panel> -->
+
+    <Panel header="Eventos" class="mt-3">
+      <FullCalendar :options="calendarOptions" />
     </Panel>
 
     <Panel header="Problemas" class="mt-3">
@@ -50,7 +54,6 @@
 </template>
 
 <script setup lang="ts">
-
 import CampoPontosTangerino, {
   DadosTangerino,
 } from "@/components/campos/CampoPontosTangerino.vue";
@@ -60,13 +63,18 @@ import "qalendar/dist/style.css";
 import { configInterface } from "qalendar/dist/src/typings/config.interface";
 import { eventInterface } from "qalendar/dist/src/typings/interfaces/event.interface";
 
-import { ref, watch } from "vue";
+import { ref, watch, computed } from "vue";
 import CampoApontamnetosMovidesk, {
   DadosMovidesk,
 } from "@/components/campos/CampoApontamnetosMovidesk.vue";
 import Offcanvas from "@/components/utils/Offcanvas.vue";
 import TabelaProblemas from "./TabelaProblemas.vue";
-import useEventBus from '@/composables/useEventBus';
+import useEventBus from "@/composables/useEventBus";
+
+import FullCalendar from "@fullcalendar/vue3";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import timeGridPlugin from "@fullcalendar/timegrid";
+import interactionPlugin from "@fullcalendar/interaction";
 
 const dadosTangerino = ref({ eventos: [] } as DadosTangerino);
 const dadosMovidesk = ref({
@@ -80,29 +88,58 @@ const { eventBus } = useEventBus();
 
 //Calendario
 
-const qalendarLightMode = ref((localStorage.getItem("darkMode") == "false"));
-eventBus.on('theme-changed', (theme) => {
-  qalendarLightMode.value = (theme == 'light');
+const calendarOptions = computed(() => ({
+  plugins: [dayGridPlugin, timeGridPlugin, interactionPlugin],
+  initialView: "timeGridWeek",
+  locale: "pt-br",
+  headerToolbar: {
+    left: "prev,next today",
+    center: "title",
+    right: "dayGridMonth,timeGridWeek,timeGridDay",
+  },
+  events: events.value,
+  slotMinTime: "07:00:00",
+  slotMaxTime: "19:00:00",
+  slotDuration: "00:20:00",
+  height: 900,
+  allDaySlot: false,
+  eventMinHeight: 0
+}));
+
+const qalendarLightMode = ref(localStorage.getItem("darkMode") == "false");
+eventBus.on("theme-changed", (theme) => {
+  qalendarLightMode.value = theme == "light";
 });
 
 watch([dadosTangerino, () => dadosMovidesk.value.eventos], () => {
   montarEventos();
 });
 
+type Event = {
+  title: string;
+  start: string;
+  end: string;
+  description?: string;
+  backgroundColor?: string;
+};
+
 const montarEventos = () => {
-  const novosEventos: eventInterface[] = [];
+  const novosEventos: Event[] = [];
   let id = 0;
 
   for (const evento of dadosTangerino.value.eventos) {
     novosEventos.push({
       title: "Trabalhando",
-      time: {
-        start: evento.inicio,
-        end: evento.fim,
-      },
-      isEditable: false,
-      color: "green",
-      id: `${id}`,
+      start: evento.inicio,
+      end: evento.fim,
+      backgroundColor: "#4caf50",
+      // time: {
+      //   start: evento.inicio,
+      //   end: evento.fim,
+      // },
+      // isEditable: false,
+      // color: "green",
+      // id: `${id}`,
     });
     id++;
   }
@@ -110,13 +147,15 @@ const montarEventos = () => {
   for (const evento of dadosMovidesk.value.eventos) {
     novosEventos.push({
       title: evento.titulo,
-      time: {
-        start: evento.inicio,
-        end: evento.fim,
-      },
-      isEditable: false,
-      color: "yellow",
-      id: `${id}`,
+      start: evento.inicio,
+      end: evento.fim,
+      // time: {
+      //   start: evento.inicio,
+      //   end: evento.fim,
+      // },
+      // isEditable: false,
+      // color: "yellow",
+      // id: `${id}`,
     });
     id++;
   }
@@ -124,7 +163,7 @@ const montarEventos = () => {
   events.value = novosEventos;
 };
 
-const events = ref([] as eventInterface[]);
+const events = ref([] as Event[]);
 
 const config = ref({
   dayBoundaries: {
